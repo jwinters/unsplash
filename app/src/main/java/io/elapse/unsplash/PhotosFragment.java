1ffe06a88d429043b8e60dac0cd398e4d808f488
Joshua Winters <winters.118@gmail.com>
2015-03-15 22:13:45 -0400

    Updates the arca version

--- app/src/main/java/io/elapse/unsplash/PhotosFragment.java
+++ app/src/main/java/io/elapse/unsplash/PhotosFragment.java
@@ -3,10 +3,7 @@ package io.elapse.unsplash;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 
@@ -16,28 +13,29 @@ import java.util.Collection;
 import io.elapse.unsplash.PhotosAdapter.ViewType;
 import io.elapse.unsplash.UnsplashContentProvider.PhotoTable;
 import io.pivotal.arca.adapters.Binding;
-import io.pivotal.arca.dispatcher.Error;
-import io.pivotal.arca.dispatcher.QueryResult;
-import io.pivotal.arca.fragments.ArcaAdapterFragment;
-import io.pivotal.arca.fragments.ArcaViewManager;
-import io.pivotal.arca.monitor.ArcaDispatcher;
+import io.pivotal.arca.fragments.ArcaFragment;
+import io.pivotal.arca.fragments.ArcaFragmentBindings;
+import io.pivotal.arca.fragments.ArcaSimpleAdapterFragment;
 
-public class PhotosFragment extends ArcaAdapterFragment implements AdapterView.OnItemClickListener {
+@ArcaFragment(
+    fragmentLayout = R.layout.fragment_photos,
+    monitor = PhotosMonitor.class
+)
+public class PhotosFragment extends ArcaSimpleAdapterFragment {
 
+    @ArcaFragmentBindings
     private static final Collection<Binding> BINDINGS = Arrays.asList(
-            ViewType.DEFAULT.newBinding(R.id.photo_image, PhotoTable.Columns.URL),
-            ViewType.LOADING.newBinding(R.id.photo_loading, PhotoTable.Columns._ID)
+        ViewType.DEFAULT.newBinding(R.id.photo_image, PhotoTable.Columns.URL),
+        ViewType.LOADING.newBinding(R.id.photo_loading, PhotoTable.Columns._ID)
     );
 
     private View mSelectedView;
-    private ArcaViewManager mManager;
 
     @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_photos, container, false);
-        final AbsListView listView = (AbsListView) view.findViewById(getAdapterViewId());
-        listView.setOnItemClickListener(this);
-        return view;
+    public CursorAdapter onCreateAdapter(final AdapterView<CursorAdapter> adapterView, final Bundle savedInstanceState) {
+        final PhotosAdapter adapter = new PhotosAdapter(getActivity(), BINDINGS);
+        adapter.setViewBinder(new PhotosViewBinder());
+        return adapter;
     }
 
     @Override
@@ -68,37 +66,10 @@ public class PhotosFragment extends ArcaAdapterFragment implements AdapterView.O
     }
 
     @Override
-    public CursorAdapter onCreateAdapter(final AdapterView<CursorAdapter> adapterView, final Bundle savedInstanceState) {
-        final PhotosAdapter adapter = new PhotosAdapter(getActivity(), BINDINGS);
-        adapter.setViewBinder(new PhotosViewBinder());
-        return adapter;
-    }
-
-    @Override
-    public ArcaDispatcher onCreateDispatcher(final Bundle savedInstanceState) {
-        final ArcaDispatcher dispatcher = super.onCreateDispatcher(savedInstanceState);
-        dispatcher.setRequestMonitor(new PhotosMonitor());
-        return dispatcher;
-    }
-
-    @Override
     public void onViewCreated(final View view, final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mManager = new ArcaViewManager(view);
-        mManager.showProgressView();
-
         execute(new PhotosQuery());
-    }
-
-    @Override
-    public void onContentChanged(final QueryResult result) {
-        mManager.checkResult(result);
-    }
-
-    @Override
-    public void onContentError(final Error error) {
-        mManager.checkError(error);
     }
 
     @Override
